456de9ed5f488dd9bb2bb94b06b47fe1038340eb
DaniiB26 <daniib1426@gmail.com>
2026-04-21 23:12:16 +0300

    Service full Implemented

---
+++ src/main/java/service/ParkingLotService.java
@@ -0,0 +1,57 @@
+package service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import model.ParkingTicket;
+import model.Vehicle;
+import pricing.PricingStrategy;
+import repository.IParkingTicketRepository;
+
+@AllArgsConstructor
+public class ParkingLotService {
+    
+    private IParkingTicketRepository parkingTicketRepository;
+    private PricingStrategy pricingStrategy;
+
+    public ParkingTicket enterParking(String licensePlate) {
+        Vehicle vehicle = new Vehicle(licensePlate);
+        ParkingTicket ticket = new ParkingTicket(vehicle, LocalDateTime.now());
+
+        return parkingTicketRepository.save(ticket);
+    }
+
+    public Double calculatePrice(UUID ticketId, LocalDateTime exitTime) {
+        ParkingTicket ticket = getTicketOrThrow(ticketId);
+
+        return pricingStrategy.calculatePrice(ticket, exitTime);
+    }
+
+    public ParkingTicket payTicket(UUID ticketId) {
+        ParkingTicket ticket = getTicketOrThrow(ticketId);
+
+        ticket.pay();
+
+        return parkingTicketRepository.save(ticket);
+    }
+
+    public ParkingTicket closeTicket(UUID ticketId) {
+        ParkingTicket ticket = getTicketOrThrow(ticketId);
+
+        ticket.close();
+
+        return parkingTicketRepository.save(ticket);
+    }
+
+    public List<ParkingTicket> getAllTickets() {
+        return parkingTicketRepository.findAll();
+    }
+
+    private ParkingTicket getTicketOrThrow(UUID ticketId) {
+        return parkingTicketRepository
+                .findById(ticketId)
+                .orElseThrow(() -> new IllegalArgumentException("Ticket not found!"));
+    }
+}
